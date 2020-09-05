2b42dabee24ec6eee226edb96cbf3c0aa0547447
Vasyl Stoliarchuk <vasyl.stoliarchuk@mcpk.tech>
2020-09-05 18:31:54 +0300

    left and right parts are drawn with with different colors

--- app/src/main/java/com/vasylstoliarchuk/waveform/MainActivity.kt
+++ app/src/main/java/com/vasylstoliarchuk/waveform/MainActivity.kt
@@ -3,13 +3,18 @@ package com.vasylstoliarchuk.waveform
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        waveform.data = floatArrayOf(0.7f, 0.1f, 0.2f, 1f, 0.9f, 0.1f, 0.7f, 0.1f, 0.2f, 1f, 0.9f, 0.1f, 0.7f, 0.1f, 0.2f, 1f, 0.9f, 0.1f, 0.7f, 0.1f, 0.2f, 1f, 0.9f, 0.1f,0.7f, 0.1f, 0.2f, 1f, 0.9f, 0.1f, 0.7f, 0.1f, 0.2f, 1f, 0.9f, 0.1f, 0.7f, 0.1f, 0.2f, 1f, 0.9f, 0.1f, 0.7f, 0.1f, 0.2f, 1f, 0.9f, 0.1f)
+        val values = (0..200).map { Random.nextFloat() }
+        val max = values.maxByOrNull { it } ?: 10000f
+
+
+        waveform.data = values.map { it / max }.toTypedArray()
 
     }
 }
--- waveform/src/main/java/com/vasylstoliarchuk/waveform/WaveformView.kt
+++ waveform/src/main/java/com/vasylstoliarchuk/waveform/WaveformView.kt
@@ -4,10 +4,9 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.view.animation.DecelerateInterpolator
+import android.view.animation.AccelerateDecelerateInterpolator
 import kotlin.math.min
 
 
@@ -15,18 +14,17 @@ class WaveformView : View {
     private val TAG = WaveformView::class.java.simpleName
 
     private var scaleFactor = 0.95f
-    var data: FloatArray = FloatArray(0)
+    var data: Array<Float> = emptyArray()
         set(value) {
             field = value
             requestLayout()
         }
 
     private var drawingPath: Path = Path()
-    private val barWidth: Int = 10
-    private val barSpacing: Int = 10
+    private val barWidth: Int = 3
+    private val barSpacing: Int = 3
     private var offsetX = 0f
         set(value) {
-            Log.d(TAG, "setOffsetX($value)")
             val validatedValue = when {
                 value > 0 -> 0f
                 value < -desiredWidth -> -desiredWidth.toFloat()
@@ -37,16 +35,24 @@ class WaveformView : View {
             invalidate()
         }
 
-    private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private val paintLeft: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
-        color = Color.BLACK
+        color = Color.parseColor("#fff7d828")
+        strokeCap = Paint.Cap.ROUND
     }
 
-    private val scaleMatrix = Matrix()
-    private val scaleRectF = RectF()
+    private val paintRight: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = Color.parseColor("#60f7d828")
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    private val clipRect = Rect()
 
     private val desiredWidth: Int
         get() = data.size * (barWidth + barSpacing) - barSpacing
+
+    private val desiredHeight = resources.getDimensionPixelSize(R.dimen.waveform_default_bar_height)
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
@@ -58,67 +64,80 @@ class WaveformView : View {
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val desiredHeight = 100
-
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
         val heightMode = MeasureSpec.getMode(heightMeasureSpec)
         val widthSize = MeasureSpec.getSize(widthMeasureSpec)
         val heightSize = MeasureSpec.getSize(heightMeasureSpec)
 
         val width = when (widthMode) {
-            MeasureSpec.EXACTLY -> widthSize;
-            MeasureSpec.AT_MOST -> min(desiredWidth, widthSize);
-            else -> desiredWidth;
+            MeasureSpec.EXACTLY -> widthSize
+            MeasureSpec.AT_MOST -> min(desiredWidth, widthSize)
+            else -> desiredWidth
         }
 
         val height = when (heightMode) {
-            MeasureSpec.EXACTLY -> heightSize;
-            MeasureSpec.AT_MOST -> min(desiredHeight, heightSize);
-            else -> desiredHeight;
+            MeasureSpec.EXACTLY -> heightSize
+            MeasureSpec.AT_MOST -> min(desiredHeight, heightSize)
+            else -> desiredHeight
         }
 
-        setMeasuredDimension(width, height);
+        setMeasuredDimension(width, height)
     }
 
 
     private fun measureDrawingPath() {
         drawingPath.reset()
-        data.forEachIndexed { index, amp ->
-            val middle = height / 2
-            val barLeft = index * (barWidth + barSpacing).toFloat()
-            val scaledHeight = height * amp / 2
-            val barTop = middle - scaledHeight
-            val barBottom = middle + scaledHeight
-            val barRight = barLeft + barWidth
+        val middle = height / 2
 
-            drawingPath.addRect(barLeft, barTop, barRight, barBottom, Path.Direction.CW)
+        data.forEachIndexed { index, amp ->
+            drawingPath.addRect(
+                index * (barWidth + barSpacing).toFloat(),
+                middle - height * amp / 2,
+                index * (barWidth + barSpacing).toFloat() + barWidth,
+                middle + height * amp / 2,
+                Path.Direction.CW
+            )
         }
         scale(scaleFactor)
         drawingPath.offset(width / 2f + offsetX, 0f)
     }
 
     override fun onDraw(canvas: Canvas) {
+        drawLeftPart(canvas)
+        drawRightPart(canvas)
+    }
+
+    private fun drawRightPart(canvas: Canvas) {
+        clipRect.set(width / 2, 0, width, height)
+        drawPart(canvas, paintRight)
+    }
+
+    private fun drawLeftPart(canvas: Canvas) {
+        clipRect.set(0, 0, width / 2, height)
+        drawPart(canvas, paintLeft)
+    }
+
+    private fun drawPart(canvas: Canvas, paint: Paint) {
+        canvas.save()
+        canvas.clipRect(clipRect)
         canvas.drawPath(drawingPath, paint)
+        canvas.restore()
     }
 
     private fun scale(scaleFactor: Float) {
-        drawingPath.computeBounds(scaleRectF, true)
-        val startMatrix = Matrix(scaleMatrix)
-        scaleMatrix.setScale(1.0f, scaleFactor, scaleRectF.centerX(), scaleRectF.centerY())
-        startAnimation(MatrixAnimation(startMatrix, scaleMatrix).apply {
-            duration = 100
-            interpolator = DecelerateInterpolator()
-        })
+        animate().scaleY(scaleFactor)
+            .setDuration(100)
+            .setInterpolator(AccelerateDecelerateInterpolator())
+            .start()
     }
 
-    private var dx = 0f
-    private var x1 = 0f
+    private var curX = 0f
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         return when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                x1 = event.x
+                curX = event.x
                 scale(1 / scaleFactor)
                 invalidate()
                 true
@@ -131,10 +150,8 @@ class WaveformView : View {
             }
 
             MotionEvent.ACTION_MOVE -> {
-                dx = event.x - x1
-                Log.d(TAG, "event.x=${event.x}, x1=$x1, dx=$dx")
-                x1 = event.x
-                offsetX += dx
+                offsetX += event.x - curX
+                curX = event.x
                 return true
             }
             else -> false
